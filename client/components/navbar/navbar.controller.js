d4b9977586ffd6826537d6d1f5c94298b02ef7b3
yan-chen <ychen.0931@gmail.com>
2015-10-14 12:33:52 -0600

    testing font

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -9,18 +9,21 @@ NavbarCtrl.$inject = [];
 function NavbarCtrl() {
   this.menu = [
     {
-      'title': 'About',
-      'icon': 'fa fa-home',
+      'title': ' About',
+      //'icon': 'fa fa-home',
+      'icon': 'glyphicon glyphicon-home',
       'state': 'about'
     },
     {
-      'title': 'Portfolio',
-      'icon': 'fa fa-folder-open',
+      'title': ' Portfolio',
+      //'icon': 'fa fa-folder-open',
+      'icon': 'glyphicon glyphicon-folder-open',
       'state': 'portfolio'
     },
     {
-      'title': 'Gallery',
-      'icon': 'fa fa-image',
+      'title': ' Gallery',
+      //'icon': 'fa fa-image',
+      'icon': 'glyphicon glyphicon-picture',
       'state': 'gallery'
     }
   ];
